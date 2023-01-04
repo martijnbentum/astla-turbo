25b4a50043bf3a3e9c79c5e29967adedb91f3f46
martijnbentum <bentummartijn@gmail.com>
2023-01-04 18:20:05 +0100

    add text module and remove django key

--- astla/settings.py
+++ astla/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^y!xz73j711c9^2h#svg5!e2!30&3b+2ttbgxhhcpefngng*8s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'texts.apps.TextsConfig',
 ]
 
 MIDDLEWARE = [
