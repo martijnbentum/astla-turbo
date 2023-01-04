9a0dc37638c425d80c04b971495c3b05b31b9c26
martijnbentum <bentummartijn@gmail.com>
2023-01-04 18:21:14 +0100

    moved excel reading function to seperate module

--- utils/collect_audio.py
+++ utils/collect_audio.py
@@ -1,26 +1,10 @@
 import glob
-from openpyxl import load_workbook
 import os
+from . import handle_dart_excel 
 
-def open_dart_excel():
-    wb = load_workbook('../DART_test_data.xlsx')
-    ws = wb['Blad1']
-    lines = list(ws.values)
-    header = lines[0]
-    data = lines[1:]
-    return wb, header, data
-
-def audio_urls(dart_data = None):
-    if not dart_data:
-        wb, header, dart_data = open_dart_excel()
-    output = []
-    for line in dart_data:
-        audio_url = line[18]
-        if audio_url not in output: output.append(audio_url)
-    return output
 
 def collect_audio(urls = None, goal_dir = '../dart_mp3/'):
-    if not urls: urls = audio_urls()
+    if not urls: urls = handle_dart_excel.audio_urls()
     if not goal_dir or not os.path.isdir(goal_dir): goal_dir = ''
     else: goal_dir = ' -P ' + goal_dir
     n_mp3_before = len(glob.glob(goal_dir + '*.mp3'))
